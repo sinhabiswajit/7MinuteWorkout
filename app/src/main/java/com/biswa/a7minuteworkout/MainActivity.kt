b07d13479212415c383259271a77dd6c4205ffd9
Biswajit Sinha <sinha.biswajit3105@gmail.com>
2023-02-21 14:06:17 +0530

    Added button ripple effect

--- app/src/main/java/com/biswa/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/com/biswa/a7minuteworkout/MainActivity.kt
@@ -2,11 +2,19 @@ package com.biswa.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val flStartButton: FrameLayout = findViewById(R.id.fl_start)
+        flStartButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Here we start the exercise",
+            Toast.LENGTH_LONG).show();
+        }
 
     }
 }
